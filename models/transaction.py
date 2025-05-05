1b1d796e9145af1ee121bc631b07d92c67104d62
Filip Kirin <filip.kirin.zg1@gmail.com>
2025-05-05 13:04:14 +0200

    Created Transaction class

---
+++ models/transaction.py
@@ -0,0 +1,26 @@
+class Transaction:
+    def __init__(self, amount, date, category, description, type):
+        self.amount = amount
+        self.date = date
+        self.category = category
+        self.description = description
+        self.type = type
+
+    def to_dict(self):
+        return {
+            "amount": self.amount,
+            "date": self.date,
+            "category": self.category,
+            "description": self.description,
+            "type": self.type
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            amount = data["amount"],
+            date = data["date"],
+            category = data["category"],
+            description = data["description"],
+            type = data["type"]
+        )
