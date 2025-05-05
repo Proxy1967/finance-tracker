a908676057e11fd0fd5a0fed5122c010231a253d
Filip Kirin <filip.kirin.zg1@gmail.com>
2025-05-05 13:05:44 +0200

    used to test

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-import tkinter as tk
+from models.transaction import Transaction
+from models.transaction_manager import TransactionManager
 
-root = tk.Tk()
-root.title("Net Worth Tracker")
-root.geometry("300x200")
-root.mainloop()
+manager = TransactionManager()
+
+# # create objects of class Transaction
+# t1 = Transaction(100, "2025-05-05", "salary", "Monthly paycheck", "income")
+# t2 = Transaction(50, "2025-05-06", "groceries", "Food", "expense")
+
+# # data = t1.to_dict()
+# # transaction = Transaction.from_dict(data)
+# # print(transaction.amount)
+
+# # add transactions to a list
+# manager.add_transaction(t1)
+# manager.add_transaction(t2)
+
+# # save the transactions to a json file
+# manager.save_to_file("data/transactions.json")
+
+manager.load_from_file("data/transactions.json")
+
+manager.list_transactions()
