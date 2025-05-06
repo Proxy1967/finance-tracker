dc64a01e257e933d2683ac212e5bf31fc20100a3
Filip Kirin <filip.kirin.zg1@gmail.com>
2025-05-06 12:34:32 +0200

    added calculations & comments

--- main.py
+++ main.py
@@ -3,21 +3,55 @@ from models.transaction_manager import TransactionManager
 
 manager = TransactionManager()
 
-# # create objects of class Transaction
-# t1 = Transaction(100, "2025-05-05", "salary", "Monthly paycheck", "income")
-# t2 = Transaction(50, "2025-05-06", "groceries", "Food", "expense")
+# create objects of class Transaction
+t1 = Transaction(1000, "2025-03-05", "salary", "Monthly paycheck", "income")
+t2 = Transaction(1000, "2025-04-05", "salary", "Monthly paycheck", "income")
+t3 = Transaction(500, "2025-05-05", "transport", "Transport", "income")
+t4 = Transaction(100, "2025-03-21", "groceries", "Food", "expense")
+t5 = Transaction(100, "2025-03-21", "misc", "Gift", "expense")
+t6 = Transaction(100, "2025-04-05", "groceries", "Food", "expense")
+t7 = Transaction(100, "2025-04-10", "groceries", "Food", "expense")
+t8 = Transaction(100, "2025-05-06", "misc", "Gift", "expense")
+t9 = Transaction(100, "2024-05-06", "misc", "Gym", "expense")
+t10 = Transaction(1000, "2024-04-05", "salary", "Monthly paycheck", "income")
 
-# # data = t1.to_dict()
-# # transaction = Transaction.from_dict(data)
-# # print(transaction.amount)
+# data = t1.to_dict()
+# transaction = Transaction.from_dict(data)
+# print(transaction.amount)
 
-# # add transactions to a list
-# manager.add_transaction(t1)
-# manager.add_transaction(t2)
+# add transactions to a list
+manager.add_transaction(t1)
+manager.add_transaction(t2)
+manager.add_transaction(t3)
+manager.add_transaction(t4)
+manager.add_transaction(t5)
+manager.add_transaction(t6)
+manager.add_transaction(t7)
+manager.add_transaction(t8)
+manager.add_transaction(t9)
+manager.add_transaction(t10)
 
-# # save the transactions to a json file
-# manager.save_to_file("data/transactions.json")
+# save the transactions to a json file
+manager.save_to_file("data/transactions.json")
 
 manager.load_from_file("data/transactions.json")
 
 manager.list_transactions()
+
+print("")
+print(manager.get_total_income())
+print(manager.get_total_expenses())
+print(manager.get_net_worth())
+print(manager.get_net_worth_by_month())
+print(manager.get_net_worth_by_year())
+print("")
+print(manager.get_expenses_by_category())
+print(manager.get_income_by_category())
+print("")
+print(manager.get_expenses_by_date())
+print("")
+print(manager.get_expenses_by_month())
+print(manager.get_income_by_month())
+print("")
+print(manager.get_expenses_by_year())
+print(manager.get_income_by_year())
--- models/transaction_manager.py
+++ models/transaction_manager.py
@@ -1,6 +1,7 @@
 from models.transaction import Transaction
 import json
 
+
 class TransactionManager:
     def __init__(self):
         self.transactions = []
@@ -10,18 +11,149 @@ class TransactionManager:
 
     def list_transactions(self):
         for num, transaction in enumerate(self.transactions, start=1):
-            print(f"{num}. {transaction.date} - {transaction.type} - €{transaction.amount} - {transaction.category} - {transaction.description}")
+            print(
+                f"{num}. {transaction.date} - {transaction.type} - {transaction.amount} - {transaction.category} ({transaction.description})"
+            )
+
+    # SAVING AND LOADING TO A JSON FILE
 
     def save_to_file(self, filename):
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             data = [transaction.to_dict() for transaction in self.transactions]
             json.dump(data, f, indent=4)
 
     def load_from_file(self, filename):
         try:
-            with open(filename, 'r') as f:
+            with open(filename, "r") as f:
                 data = json.load(f)
                 self.transactions = [Transaction.from_dict(item) for item in data]
         except FileNotFoundError:
             print(f"No file found at {filename}. Starting with an empty list.")
             self.transactions = []
+
+    # CALCULATIONS FOR EXPENSES
+
+    def get_total_expenses(self):
+        total = 0
+        for t in self.transactions:
+            if t.type == "expense":
+                total += t.amount
+        return total
+
+    def get_expenses_by_category(self):
+        total_by_category = {}
+        for t in self.transactions:
+            if t.type == "expense":
+                if t.category not in total_by_category:
+                    total_by_category[t.category] = 0
+                total_by_category[t.category] += t.amount
+        return total_by_category
+
+    def get_expenses_by_date(self):
+        total_by_date = {}
+        for t in self.transactions:
+            if t.type == "expense":
+                if t.date not in total_by_date:
+                    total_by_date[t.date] = 0
+                total_by_date[t.date] += t.amount
+        return total_by_date
+
+    def get_expenses_by_month(self):
+        total_by_month = {}
+        for t in self.transactions:
+            if t.type == "expense":
+                month = t.date[:7]
+                if month not in total_by_month:
+                    total_by_month[month] = 0
+                total_by_month[month] += t.amount
+        return total_by_month
+
+    def get_expenses_by_year(self):
+        total_by_year = {}
+        for t in self.transactions:
+            if t.type == "expense":
+                year = t.date[:4]
+                if year not in total_by_year:
+                    total_by_year[year] = 0
+                total_by_year[year] += t.amount
+        return total_by_year
+
+    # CALCULATIONS FOR INCOME
+
+    def get_total_income(self):
+        total = 0
+        for t in self.transactions:
+            if t.type == "income":
+                total += t.amount
+        return total
+
+    def get_income_by_category(self):
+        total_by_category = {}
+        for t in self.transactions:
+            if t.type == "income":
+                if t.category not in total_by_category:
+                    total_by_category[t.category] = 0
+                total_by_category[t.category] += t.amount
+        return total_by_category
+
+    def get_income_by_month(self):
+        total_by_month = {}
+        for t in self.transactions:
+            if t.type == "income":
+                month = t.date[:7]
+                if month not in total_by_month:
+                    total_by_month[month] = 0
+                total_by_month[month] += t.amount
+        return total_by_month
+
+    def get_income_by_year(self):
+        total_by_year = {}
+        for t in self.transactions:
+            if t.type == "income":
+                year = t.date[:4]
+                if year not in total_by_year:
+                    total_by_year[year] = 0
+                total_by_year[year] += t.amount
+        return total_by_year
+
+    # CALCULATIONS FOR NET WORTH
+
+    def get_net_worth(self):
+        return self.get_total_income() + self.get_total_expenses()
+
+    def get_net_worth_by_month(self):
+        net_worth_by_month = {}
+        total_income_by_month = self.get_income_by_month()
+        total_expenses_by_month = self.get_expenses_by_month()
+        net_worth_by_month.update(
+            self.merge_dicts(total_income_by_month, total_expenses_by_month)
+        )
+        return net_worth_by_month
+
+    def get_net_worth_by_year(self):
+        net_worth_by_year = {}
+        total_income_by_year = self.get_income_by_year()
+        total_expenses_by_year = self.get_expenses_by_year()
+        net_worth_by_year.update(
+            self.merge_dicts(total_income_by_year, total_expenses_by_year)
+        )
+        return net_worth_by_year
+
+    # HELPER FUNCTIONS
+
+    def merge_dicts(self, dict1, dict2):
+        merged_dict = {}
+
+        for key in dict1:
+            if key in dict2:
+                new_value = dict1[key] + dict2[key]
+            else:
+                new_value = dict1[key]
+
+            merged_dict[key] = new_value
+
+        for key in dict2:
+            if key not in merged_dict:
+                merged_dict[key] = dict2[key]
+
+        return merged_dict
